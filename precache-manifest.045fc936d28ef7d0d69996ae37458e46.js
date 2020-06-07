a15005880dcfb1b1fab98af202df9b5727ef003e
Shreya Pandey <shrpande1@publicisgroupe.net>
2020-06-07 18:19:05 +0400

    Updates

---
+++ precache-manifest.045fc936d28ef7d0d69996ae37458e46.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d4c8450ee2dcb60a4967dec42dde52d",
+    "url": "/Rick_-_Morty/index.html"
+  },
+  {
+    "revision": "ecbdd4304873da4fef53",
+    "url": "/Rick_-_Morty/static/css/main.737a09e7.chunk.css"
+  },
+  {
+    "revision": "302105dcd98b4491f320",
+    "url": "/Rick_-_Morty/static/js/2.cf9900cf.chunk.js"
+  },
+  {
+    "revision": "2069192f75c6f3fb6247231eea52f248",
+    "url": "/Rick_-_Morty/static/js/2.cf9900cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ecbdd4304873da4fef53",
+    "url": "/Rick_-_Morty/static/js/main.10ec188f.chunk.js"
+  },
+  {
+    "revision": "c33d95d4a98dae70a29b",
+    "url": "/Rick_-_Morty/static/js/runtime-main.b85ab48b.js"
+  }
+]);
